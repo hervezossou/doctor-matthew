1c993c34185342c3c88c21eed964bfe14ed7874c
Hervé Zossou <hervezossou8@gmail.com>
2024-10-08 19:25:18 +0100

    WhatsApp Blue Icon in Appointment section

--- src/data/appointment.js
+++ src/data/appointment.js
@@ -1,9 +1,9 @@
-import whatsappIcon from '../assets/icons/whatsapp.png'
+import whatsappBlue from '../assets/icons/whatsapp-blue.png'
 
 export const appointment = {
     "title": "Book an appointment",
     "subtitle": "Schedule a virtual or presential appointment today",
     "paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas egestas viverra turpis habitant eu sociis fermentum felis.",
-    "icon": whatsappIcon,
+    "icon": whatsappBlue,
     "label": "Book an appointment"
 }
