2527fb982330782510e82ec0cbda87702cf9c5b7
Hervé Zossou <hervezossou8@gmail.com>
2024-10-08 16:55:45 +0100

    Updating services file with image

--- src/data/services.js
+++ src/data/services.js
@@ -1,6 +1,11 @@
 import whatsappIcon from '../assets/icons/whatsapp.png'
 import servicesImage from '../assets/images/services-image.png'
 import generalImage from '../assets/images/general.png'
+import pediatrics from '../assets/images/pediatrics.png'
+import nutritional from '../assets/images/nutritional.png'
+import ophthalmology from '../assets/images/ophthalmology.png'
+import cardiology from '../assets/images/cardiology.png'
+import orthopedics from '../assets/images/orthopedics.png'
 
 export const servicesHeader = {
     "title": "Services",
@@ -21,35 +26,35 @@ export const services = [
         "label": "Book an appointment"
     },
     {
-        "image": generalImage,
+        "image": pediatrics,
         "title": "Pediatrics", 
         "description": "Lorem ipsum dolor sit amet, consecte tur adipiscing elit aliquet iTristique id nibh lobortis nunc",
         "icon": whatsappIcon,
         "label": "Book an appointment"
     },
     {
-        "image": generalImage,
+        "image": nutritional,
         "title": "Nutritional", 
         "description": "Lorem ipsum dolor sit amet, consecte tur adipiscing elit aliquet iTristique id nibh lobortis nunc",
         "icon": whatsappIcon,
         "label": "Book an appointment"
     },
     {
-        "image": generalImage,
+        "image": cardiology,
         "title": "Cardiology", 
         "description": "Lorem ipsum dolor sit amet, consecte tur adipiscing elit aliquet iTristique id nibh lobortis nunc",
         "icon": whatsappIcon,
         "label": "Book an appointment"
     },
     {
-        "image": generalImage,
+        "image": ophthalmology,
         "title": "Ophthalmology", 
         "description": "Lorem ipsum dolor sit amet, consecte tur adipiscing elit aliquet iTristique id nibh lobortis nunc",
         "icon": whatsappIcon,
         "label": "Book an appointment"
     },
     {
-        "image": generalImage,
+        "image": orthopedics,
         "title": "Orthopedics", 
         "description": "Lorem ipsum dolor sit amet, consecte tur adipiscing elit aliquet iTristique id nibh lobortis nunc",
         "icon": whatsappIcon,
