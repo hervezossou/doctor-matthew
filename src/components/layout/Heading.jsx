814fe7a3998d6805b4010d6e758a390f0665ce59
Hervé Zossou <hervezossou8@gmail.com>
2024-10-10 15:17:43 +0100

    Appointment Section

--- src/components/layout/Heading.jsx
+++ src/components/layout/Heading.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types'
 
 export default function Heading ({title, paragraph}) {
  return (
-    <div>
-        <h2>{title}</h2>
-        <p>{paragraph}</p>
+    <div className='flex flex-col items-center justify-center gap-4 mb-6'>
+        <h2 className='font-semibold text-lg text-center text-body'>{title}</h2>
+        <p className='font-sans text-sm text-center text-paragraph'>{paragraph}</p>
     </div>
  )
 }
--- src/components/sections/Appointment.jsx
+++ src/components/sections/Appointment.jsx
@@ -4,9 +4,9 @@ import { appointment } from "../../data/appointment"
 
 export default function AppointmentSection () {
     return (
-        <div className="bg-pure-white flex flex-col gap-10 p-6">
+        <div className="bg-pure-white flex flex-col items-center justify-between gap-10 p-6">
             <h2 className="font-medium text-title text-base font-body">{appointment.title}</h2>
-            <div className="appointment-cta">
+            <div className="bg-gradient-to-r from-pure-blue to-light-blue flex flex-col items-center justify-center gap-4 p-4 rounded-lg">
                 <Heading title={appointment.subtitle} paragraph={appointment.paragraph} />
                 <Button icon={appointment.icon} label={appointment.label}/>
             </div>
