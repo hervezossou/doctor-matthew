c182d5be520352b764bfe257c2a8f9b382764ea5
Hervé Zossou <hervezossou8@gmail.com>
2024-10-10 10:10:22 +0100

    Styling FlexCard Component

--- src/components/layout/FlexCard.jsx
+++ src/components/layout/FlexCard.jsx
@@ -4,10 +4,10 @@ import Button from '../ui/Button'
 export default function FlexCard ({title, subtitle, paragraph, icon, label}) {
  return (
 
-    <div>
-        <h2>{title}</h2>
-        <span>{subtitle}</span>
-        <p>{paragraph}</p>
+    <div className="flex flex-col items-start justify-between gap-5">
+        <h2 className='font-medium text-title text-base font-body'>{title}</h2>
+        <span className='font-bold text-2xl text-subtitle leading-tight'>{subtitle}</span>
+        <p className='font-sans text-paragraph text-sm'>{paragraph}</p>
         {(label && icon) && (
             <Button icon={icon} label={label}/>
         )}
