d3bd39358023e8cfd8689e3c1173b17ff9b55172
Hervé Zossou <hervezossou8@gmail.com>
2024-10-11 07:53:32 +0100

    AppointmentSection layout for tablet and desktop

--- src/components/sections/Appointment.jsx
+++ src/components/sections/Appointment.jsx
@@ -4,9 +4,9 @@ import { appointment } from "../../data/appointment"
 
 export default function AppointmentSection () {
     return (
-        <div className="flex flex-col items-center justify-between gap-10 bg-pure-white p-6">
+        <div className="flex flex-col items-center justify-between gap-10 bg-pure-white p-6 md:p-16 lg:w-screen lg:px-32">
             <h2 className="font-body text-base font-medium text-title">{appointment.title}</h2>
-            <div className="flex flex-col items-center justify-center gap-4 rounded-lg bg-gradient-to-r from-pure-blue to-light-blue p-4">
+            <div className="flex flex-col items-center justify-center gap-4 rounded-lg bg-gradient-to-r from-pure-blue to-light-blue p-4 lg:w-full lg:rounded-2xl lg:p-12">
                 <Heading title={appointment.subtitle} paragraph={appointment.paragraph} />
                 <Button icon={appointment.icon} label={appointment.label}/>
             </div>
