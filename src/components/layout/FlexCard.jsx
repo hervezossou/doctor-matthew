5c99a4891dc3ea87143b716ff65f749269e742ac
Hervé Zossou <hervezossou8@gmail.com>
2024-10-11 16:01:46 +0100

    FlexCard variants

--- src/components/layout/FlexCard.jsx
+++ src/components/layout/FlexCard.jsx
@@ -1,21 +1,22 @@
 import PropTypes from 'prop-types'
 import Button from '../ui/Button'
 
-export default function FlexCard ({title, subtitle, paragraph, icon, label, variant ="primary"}) {
+export default function FlexCard ({title, subtitle, paragraph, icon, label, variant}) {
     const variants = {
-        "primary": "text-lg lg:text-3xl",
-        "secondary": "text-base lg:text-xl"
+        "primary": "text-3xl md:text-4xl xl:text-5xl",
+        "secondary": "text-xl md:text-2xl xl:text-3xl"
     }
-    return (
-        <div className="flex flex-col items-center gap-5 md:w-1/2 md:items-start md:gap-3">
-            <h2 className={`text-center font-body font-medium ${variants[variant]} text-title md:text-left`}>{title}</h2>
-            <span className='text-center font-body text-2xl font-bold leading-tight text-subtitle md:text-left md:text-3xl lg:text-4xl xl:text-5xl'>{subtitle}</span>
-            <p className='text-center font-sans text-sm font-normal text-paragraph md:text-justify lg:text-base xl:text-lg'>{paragraph}</p>
-            {(label && icon) && (
-                <Button icon={icon} label={label} variant='primary'/>
-            )}
-        </div>
-    )
+ return (
+
+    <div className="flex flex-col items-center gap-5 md:w-1/2 md:items-start md:gap-3">
+        <h2 className='text-center font-body text-base font-medium text-title md:text-left lg:text-2xl'>{title}</h2>
+        <span className={`text-center font-body font-bold ${variants[variant]} leading-tight text-subtitle md:text-left`}>{subtitle}</span>
+        <p className='text-center font-sans text-sm font-normal text-paragraph md:text-justify lg:text-base xl:text-lg'>{paragraph}</p>
+        {(label && icon) && (
+            <Button icon={icon} label={label} variant='primary'/>
+        )}
+    </div>
+ )
 }
 
 FlexCard.propTypes = {
