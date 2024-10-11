ce34095d4ebf02fd3cc0e0fb9dfc8d77998d22f1
Hervé Zossou <hervezossou8@gmail.com>
2024-10-11 13:47:03 +0100

    Responsive Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types'
 
 export default function Button ({icon, label}) {
     return (
-        <a className="flex h-12 w-auto items-center justify-center gap-2.5 rounded-3xl bg-gradient-to-r from-primary-blue to-primary-violet px-7 py-4 drop-shadow-3xl md:my-3" href="#" target="_blank">
+        <a className="flex h-12 w-auto items-center justify-center gap-2.5 rounded-3xl bg-gradient-to-r from-primary-blue to-primary-violet px-7 py-4 drop-shadow-3xl md:my-3 lg:w-80" href="#" target="_blank">
             {icon && (
                 <img src={icon} alt="" />
             )}
-            <span className='font-sans text-sm font-semibold text-pure-white'>{label}</span>
+            <span className='font-sans text-sm font-semibold text-pure-white md:text-base lg:text-lg'>{label}</span>
         </a>
     )
 }
