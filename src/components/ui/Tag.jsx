9b248da7a343ee899bc97d2073d79f124c12e8a2
Hervé Zossou <hervezossou8@gmail.com>
2024-10-11 16:41:22 +0100

    Adjusting Tag Component for all screen size

--- src/components/ui/Tag.jsx
+++ src/components/ui/Tag.jsx
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types'
 
 export default function Tag ({icon, title, description}) {
     return (
-        // eslint-disable-next-line tailwindcss/no-custom-classname
-        <div className='md:w-p6 h-18 flex w-80 items-center justify-center gap-4 rounded-xl bg-gradient-to-r from-pure-blue to-light-blue px-5 py-2'>
+        <div className='flex h-16 w-72 items-center justify-center gap-4 rounded-xl bg-gradient-to-r from-pure-blue to-light-blue px-5 py-2'>
             {icon && (
                 <img src={icon} />
             )}
             <div className='flex flex-col'>
                 <h3 className='font-body text-sm font-semibold text-pure-white'>{title}</h3>
-                {description && <p className='h-5 w-48 font-sans text-xs font-normal text-pure-white'>{description}</p>}
+                {description && <p className='h-8 w-48 font-sans text-xs font-normal text-pure-white'>{description}</p>}
             </div>
         </div>
     )
@@ -18,5 +17,5 @@ export default function Tag ({icon, title, description}) {
 Tag.propTypes = {
     icon: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    description: PropTypes.string
+    description: PropTypes.string,
 }
