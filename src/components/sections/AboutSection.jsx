bf14e94b66526141cb9b10a7eecbee5ebca76f52
Hervé Zossou <hervezossou8@gmail.com>
2024-10-11 22:48:51 +0100

    items-center in about section

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -4,7 +4,7 @@ import { about } from "../../data/about"
 
 export default function AboutSection () {
     return (
-        <div className="my-10 flex flex-col gap-10 p-6 md:flex-row-reverse md:p-16 lg:px-24 xl:px-40">
+        <div className="my-10 flex flex-col gap-10 p-6 md:flex-row-reverse md:items-center md:p-16 lg:px-24 xl:px-40">
             <FlexCard 
                     title={about.title} 
                     subtitle={about.subtitle} 
