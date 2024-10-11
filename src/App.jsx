2a399ca69b673213a47ff090b4f62c40ab080d32
Hervé Zossou <hervezossou8@gmail.com>
2024-10-11 07:10:54 +0100

    Header Sampler

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from './components/sections/Header'
 import HeroSection from './components/sections/HeroSection'
 import AboutSection from './components/sections/AboutSection'
 import ServicesSection from './components/sections/ServicesSection'
@@ -9,6 +10,7 @@ import Footer from './components/sections/Footer'
 function App() {
   return (
     <>
+      <Header />
       <main>
         <HeroSection />
         <AboutSection />
--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,7 +1,24 @@
 export default function Header () {
     return (
-        <header>
-
+        <header className="bg-alice-blue py-2">
+            <div className="container mx-auto px-4">
+                <nav className="flex items-center justify-between py-4">
+                    <div>
+                        <h1 className="text-2xl font-bold">Doctor +</h1>
+                    </div>
+                    <div className="hidden space-x-4 md:flex">
+                        <a href="#" className="hover:text-primary-blue">Home</a>
+                        <a href="#" className="hover:text-primary-blue">About</a>
+                        <a href="#" className="hover:text-primary-blue">Services</a>
+                        <a href="#" className="hover:text-primary-blue">Contact</a>
+                    </div>
+                    <button className="md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
+                </nav>
+            </div>
         </header>
     )
 } 
